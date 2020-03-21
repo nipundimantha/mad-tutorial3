8bcc1330676e4054ae64db7f1cd86a1ade61f069
donttrust@123 <nipundimantha136@gmail.com>
2020-03-22 03:54:21 +0530

    fragments in android

---
+++ app/src/main/java/com/madtutorial3/androidfragments/MainActivity.java
@@ -0,0 +1,43 @@
+package com.madtutorial3.androidfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+/**
+ * The main program implements an application that
+ * simply displays the fragments to the standard output.
+ *
+ * @author  Dimantha H.V.N
+ * @version 1.0
+ * @since   2020-03-22
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void changeFragment(View view){
+        Fragment fragment;
+        if (view == findViewById(R.id.btnFragment1)){
+            fragment = new Fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault,fragment);
+            ft.commit();
+        }
+        if (view == findViewById(R.id.btnFragment2)){
+            fragment = new Fragment2();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault,fragment);
+            ft.commit();
+        }
+    }
+}
